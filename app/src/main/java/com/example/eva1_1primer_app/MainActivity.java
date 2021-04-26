bde049a80dd7ad415bcff542fb9dc0e620c9f570
OscarIvanRosales20 <L18550734@chihuahua2.tecnm.mx>
2021-04-25 20:57:21 -0600

    Initial commit

---
+++ app/src/main/java/com/example/eva1_1primer_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.eva1_1primer_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { //Ciclo de vida de app 1 - Evento
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
